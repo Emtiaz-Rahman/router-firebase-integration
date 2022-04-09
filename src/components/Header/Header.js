759d65791743e9f64a9080243efa0715dc70a29d
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-04-09 12:36:19 +0600

    initialize the project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
+
 
 const Header = () => {
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <link to='/login'>Login</link>
+        <div className='header'>
+            <nav>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+
+            </nav>
+            
         </div>
     );
 };
